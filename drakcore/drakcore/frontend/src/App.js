bb0c8b9f8252087295b9d4717a55f6b659f8efd5
Michał Leszczyński <michal.leszczynski@cert.pl>
2020-04-23 04:11:51 +0200

    Change web UI template (#41)

--- drakcore/drakcore/frontend/src/App.js
+++ drakcore/drakcore/frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import {Component} from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  withRouter
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    withRouter
 } from "react-router-dom";
 import AnalysisList from "./AnalysisList";
 import AnalysisMain from "./AnalysisMain";
 import AnalysisStatus from "./AnalysisStatus";
+import UploadSample from "./UploadSample";
 
 
 class NavBtns extends Component {
@@ -20,14 +21,18 @@ class NavBtns extends Component {
             analysis_id = this.props.location.pathname.split("/")[2];
         }
 
-        let nav_btns = <ul className="navbar-nav mr-auto" />;
+        let nav_btns = <div/>;
 
         if (analysis_id) {
-            nav_btns = <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                    <Link to={"/analysis/" + analysis_id} className="nav-link">Analysis</Link>
+            nav_btns = <div>
+                <li className="side-nav-title side-nav-item">Analysis</li>
+                <li className="side-nav-item">
+                    <Link to={"/analysis/" + analysis_id} className="side-nav-link">
+                        <i className="uil-clipboard-alt"/>
+                        <span> Report </span>
+                    </Link>
                 </li>
-            </ul>;
+            </div>;
         }
 
         return nav_btns;
@@ -40,26 +45,69 @@ class App extends Component {
         const NavBtnsWithRouter = withRouter(NavBtns);
 
         return <Router>
-            <div>
-                <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                    <Link className="navbar-brand" to="/">DRAKVUF Sandbox</Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" />
-                    </button>
+            <div className="wrapper">
+                <div className="left-side-menu">
+                    <a href="/" className="logo text-center logo-light">
+                        <span className="logo-lg">
+                            <img src="/assets/images/logo.png" alt=""/>
+                        </span>
+                        <span className="logo-sm">
+                            <img src="/assets/images/logo.png" alt="" height="16"/>
+                        </span>
+                    </a>
 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <NavBtnsWithRouter />
+                    <div className="h-100" id="left-side-menu-container" data-simplebar>
+                        <ul className="metismenu side-nav">
+                            <li className="side-nav-title side-nav-item">Sandbox</li>
+                            <li className="side-nav-item">
+                                <Link to={"/upload"} className="side-nav-link">
+                                    <i className="uil-file-upload"/>
+                                    <span> Upload sample </span>
+                                </Link>
+                            </li>
+                            <li className="side-nav-item">
+                                <Link to={"/"} className="side-nav-link">
+                                    <i className="uil-folder"/>
+                                    <span> Analyses </span>
+                                </Link>
+                            </li>
+
+                            <NavBtnsWithRouter/>
+                        </ul>
+                        <div className="clearfix"/>
                     </div>
-                </nav>
 
-                <Switch>
-                    <Route path="/progress/:analysis" component={AnalysisStatus} />
-                    <Route path="/analysis/:analysis" component={AnalysisMain} />
-                    <Route path="/" exact component={AnalysisList} />
-                </Switch>
+                </div>
             </div>
+
+            <div className="content-page">
+                <div className="content">
+                    <Switch>
+                        <Route path="/progress/:analysis" component={AnalysisStatus}/>
+                        <Route path="/analysis/:analysis" component={AnalysisMain}/>
+                        <Route path="/upload" exact component={UploadSample}/>
+                        <Route path="/" exact component={AnalysisList}/>
+                    </Switch>
+                </div>
+            </div>
+
+            <footer className="footer">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6">
+                            DRAKVUF Sandbox (C) 2020 <a href="https://cert.pl/">CERT Polska</a><br/>
+                            DRAKVUF (C) 2014-2020 <a href="https://tklengyel.com/">Tamas K Lengyel</a>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="text-md-right footer-links d-none d-md-block">
+                                <a href="https://github.com/CERT-Polska/drakvuf-sandbox">CERT-Polska / drakvuf-sandbox</a>
+                                <a href="https://github.com/tklengyel/drakvuf">tklengyel / drakvuf</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+
         </Router>;
     }
 }
---
+++ drakcore/drakcore/frontend/src/UploadSample.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Component } from "react";
+import "./App.css";
+
+class UploadSample extends Component {
+    render() {
+        return <div className="App container-fluid">
+
+            <div className="page-title-box">
+                <h4 className="page-title">Upload sample</h4>
+            </div>
+
+            <form action="/upload" method="POST" encType="multipart/form-data">
+                <input type="file" name="file" />
+                <button type="submit">Upload</button>
+            </form>
+        </div>;
+    }
+}
+
+export default UploadSample;
