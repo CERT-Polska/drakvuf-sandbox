5380bcd0826c338add56683695c9732f7871cda7
Hubert Jasudowicz <chivay@users.noreply.github.com>
2020-04-29 18:35:30 +0200

    Reformat UI with Prettier (#69)

--- drakcore/drakcore/frontend/src/AnalysisList.js
+++ drakcore/drakcore/frontend/src/AnalysisList.js
@@ -5,62 +5,72 @@ import "./App.css";
 import api from "./api";
 
 class AnalysisList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            analyses: []
-        };
-    }
+    this.state = {
+      analyses: [],
+    };
+  }
 
-    async componentDidMount() {
-        const response = await api.getList();
-        if (response.data)
-            this.setState({ analyses: response.data });
-    }
+  async componentDidMount() {
+    const response = await api.getList();
+    if (response.data) this.setState({ analyses: response.data });
+  }
 
-    formatTimestamp(ts) {
-        return (new Date(ts * 1000)).toISOString()
-            .replace('T', ' ')
-            .replace('.000Z', '');
-    }
+  formatTimestamp(ts) {
+    return new Date(ts * 1000)
+      .toISOString()
+      .replace("T", " ")
+      .replace(".000Z", "");
+  }
 
-    render() {
-        return <div className="App container-fluid">
+  render() {
+    return (
+      <div className="App container-fluid">
+        <div className="page-title-box">
+          <h4 className="page-title">Analyses</h4>
+        </div>
 
-            <div className="page-title-box">
-                <h4 className="page-title">Analyses</h4>
-            </div>
-
-            <table className="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>Sample SHA256</th>
-                        <th>File type</th>
-                        <th>Started</th>
-                        <th>Finished</th>
-                        <th>Start command</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    this.state.analyses.map((val) => {
-                        return <tr key={val.id}>
-                            <td className="text-hash"><Link to={"/analysis/" + val.id}>{val.meta.sample_sha256}</Link></td>
-                            <td>{val.meta.magic_output}</td>
-                            <td>{this.formatTimestamp(val.meta.time_started)}</td>
-                            <td>{this.formatTimestamp(val.meta.time_finished)}</td>
-                            <td><code>{val.meta.start_command}</code></td>
-                            <td><Link to={"/analysis/" + val.id}>
-                                <i className="uil uil-document"/> View</Link></td>
-                        </tr>;
-                    })
-                }
-                </tbody>
-            </table>
-        </div>;
-    }
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Sample SHA256</th>
+              <th>File type</th>
+              <th>Started</th>
+              <th>Finished</th>
+              <th>Start command</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.analyses.map((val) => {
+              return (
+                <tr key={val.id}>
+                  <td className="text-hash">
+                    <Link to={"/analysis/" + val.id}>
+                      {val.meta.sample_sha256}
+                    </Link>
+                  </td>
+                  <td>{val.meta.magic_output}</td>
+                  <td>{this.formatTimestamp(val.meta.time_started)}</td>
+                  <td>{this.formatTimestamp(val.meta.time_finished)}</td>
+                  <td>
+                    <code>{val.meta.start_command}</code>
+                  </td>
+                  <td>
+                    <Link to={"/analysis/" + val.id}>
+                      <i className="uil uil-document" /> View
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default AnalysisList;
--- drakcore/drakcore/frontend/src/api/index.js
+++ drakcore/drakcore/frontend/src/api/index.js
@@ -4,25 +4,25 @@ import axios from "axios";
 // axios.defaults.baseURL = '';
 
 export default {
-    async getList() {
-        return axios.get("/list");
-    },
-    async getLog(analysis, type) {
-        return axios.get(`/logs/${analysis}/${type}`);
-    },
-    async listLogs(analysis) {
-        return axios.get(`/logs/${analysis}`);
-    },
-    async getStatus(analysis) {
-        return axios.get(`/status/${analysis}`);
-    },
-    async getGraph(analysis) {
-        return axios.get(`/graph/${analysis}`);
-    },
-    async getProcessTree(analysis) {
-        return axios.get(`/processed/${analysis}/process_tree.json`);
-    },
-    async query(q) {
-        return axios.get("/query", { params: { q: q } });
-    }
+  async getList() {
+    return axios.get("/list");
+  },
+  async getLog(analysis, type) {
+    return axios.get(`/logs/${analysis}/${type}`);
+  },
+  async listLogs(analysis) {
+    return axios.get(`/logs/${analysis}`);
+  },
+  async getStatus(analysis) {
+    return axios.get(`/status/${analysis}`);
+  },
+  async getGraph(analysis) {
+    return axios.get(`/graph/${analysis}`);
+  },
+  async getProcessTree(analysis) {
+    return axios.get(`/processed/${analysis}/process_tree.json`);
+  },
+  async query(q) {
+    return axios.get("/query", { params: { q: q } });
+  },
 };
