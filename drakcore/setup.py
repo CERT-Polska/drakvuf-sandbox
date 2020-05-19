87a675dffef91c0a9ae78eb7c5300504e70cbab1
Michał Leszczyński <michal.leszczynski@cert.pl>
2020-05-19 14:13:43 +0200

    Bump version to 0.5.0 (#116)

--- drakcore/setup.py
+++ drakcore/setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name="drakcore",
-    version="0.4.0",
+    version="0.5.0",
     description="DRAKVUF Sandbox Core",
     package_dir={"drakcore": "drakcore"},
     packages=["drakcore", "drakcore.postprocess"],
