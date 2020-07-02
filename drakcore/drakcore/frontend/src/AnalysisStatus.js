7ffa4f9d449c33b7b619074c8dead3105ad396c8
Adam Kliś <bonus@cert.pl>
2020-07-02 12:56:28 +0200

    add VNC client

--- drakcore/drakcore/frontend/src/AnalysisStatus.js
+++ drakcore/drakcore/frontend/src/AnalysisStatus.js
@@ -3,6 +3,52 @@ import { Component } from "react";
 import { Redirect } from "react-router-dom";
 import "./App.css";
 import api from "./api";
+import RFB from '@novnc/novnc'
+
+class PasswordField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(event) {
+    if (event.target.name === "password")
+      this.setState({ value: event.target.value });
+    this.props.onInput(event);
+  }
+
+  render() {
+    return (
+      <div className="form-group" style={this.props.style}>
+        <label>{this.props.label}</label>
+        <div className="input-group">
+          <input
+            type="password"
+            name="password"
+            className="form-control"
+            placeholder={this.props.placeholder}
+            onChange={this.handleInput}
+          />
+          <button
+            type="button"
+            name="submit"
+            className="btn btn-primary"
+            onClick={this.handleInput}
+          >Open VNC</button>
+        </div>
+        {this.props.hint !== undefined ? (
+          <small className="form-text text-muted">{this.props.hint}</small>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+}
 
 class AnalysisStatus extends Component {
   constructor(props) {
@@ -11,10 +57,16 @@ class AnalysisStatus extends Component {
     this.state = {
       status: "unknown",
       spinner: "oO",
+      password: "",
+      error: null,
+      vnc_started: false,
       updated: false,
     };
 
     this.timerID = null;
+
+    this.handleInput = this.handleInput.bind(this);
+    this.createConnection = this.createConnection.bind(this);
   }
 
   componentDidMount() {
@@ -35,10 +87,41 @@ class AnalysisStatus extends Component {
     }, 1000);
   }
 
+  createConnection() {
+    let rfb = null
+    try {
+      rfb = new RFB(document.getElementById('noVNC-canvas'), `ws://:6301`, { credentials: { password: this.state.password }})
+      rfb.addEventListener('connect', () => {
+          this.setState({ vnc_started: true, error: null });
+          rfb.focus();
+          const canvas = document.getElementById("noVNC-canvas").firstChild.firstChild;
+          canvas.style.width = "auto";
+          canvas.style.height = "auto";
+      });
+      rfb.addEventListener('disconnect', () => this.setState({ vnc_started: false }));
+      rfb.addEventListener('securityfailure', (err) => this.setState({ error: err.detail.reason }))  // TODO: show error
+      rfb.scaleViewport = true;
+      rfb.resizeSession = true;
+    } catch (err) {
+      console.error(`Unable to create RFB client: ${err}`)
+    }
+  
+    return rfb
+  }
+
   componentWillUnmount() {
     if (this.timerID) {
       clearTimeout(this.timerID);
     }
+  }
+
+  handleInput(event) {
+    if (event.target.name === "password")
+      this.setState({ password: event.target.value });
+    else if (event.target.name === "submit")
+      this.createConnection();
+    else
+      console.log("Unexpected event: ", event);
   }
 
   render() {
@@ -63,6 +146,20 @@ class AnalysisStatus extends Component {
             style={{ width: "100%" }}
           />
         </div>
+
+        <br/>
+
+        <PasswordField
+            label="VNC password"
+            onInput={this.handleInput}
+            placeholder="password"
+            hint="VNC password is generated once for all VMs and is kept in /etc/drakrun/scripts/cfg.template"
+            style={{ display: this.state.vnc_started ? 'none' : 'block' }}
+        />
+
+        { this.state.error && <div className="alert alert-danger">{ this.state.error }</div> }
+
+        <div id='noVNC-canvas' style={{display: this.state.vnc_started ? 'block' : 'none'}} />
       </div>
     );
   }
