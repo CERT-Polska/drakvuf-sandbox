9425fd646d0681f4cf576889a7b6823e4d4313dc
Adam Kliś <adam.klis@cert.pl>
2020-07-14 02:03:34 +0000

    Bump version to 0.7.0 (#165)

--- drakrun/setup.py
+++ drakrun/setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name="drakrun",
-    version="0.6.0",
+    version="0.7.0",
     description="DRAKRUN",
     package_dir={"drakrun": "drakrun"},
     packages=["drakrun"],
