a4b803693511c2b068755d93b021219b92d321e7
Michał Leszczyński <michal.leszczynski@cert.pl>
2020-08-08 13:50:08 +0200

    bump DRAKVUF deb in conftest

--- test/conftest.py
+++ test/conftest.py
@@ -44,7 +44,7 @@ DRAKMON_DEPS = [
     "ntfs-3g",
 ]
 
-DRAKVUF_BUNDLE_URL = "https://debs.icedev.pl/manual/patch20200806/drakvuf-bundle-0.8-git20200806003537+8a7d14d-1-generic.deb"
+DRAKVUF_BUNDLE_URL = "https://github.com/tklengyel/drakvuf-builds/releases/download/20200808010954-3e07fa9/drakvuf-bundle-0.8-git20200807205720+3e07fa9-1-generic.deb"
 DRAKVUF_DEPS = [
     "libpixman-1-0",
     "libpng16-16",
