6a80c40db498d3f89dcefaeb509279c6dafa35f1
Konstanty Cieslinski <konstanty.cieslinski@cert.pl>
2020-08-13 11:38:09 +0200

    Added extensions to drakcore and drakpush.

--- drakrun/drakrun/drakpush.py
+++ drakrun/drakrun/drakpush.py
@@ -8,16 +8,29 @@ from drakrun.config import ETC_DIR
 def main():
     parser = argparse.ArgumentParser(description='Push sample to the karton')
     parser.add_argument('sample', help='Path to the sample')
+    parser.add_argument('--start_command', help='e.g. start %f, %f will be replaced by file name', required=False)
     args = parser.parse_args()
 
     conf = Config(os.path.join(ETC_DIR, 'config.ini'))
     producer = Producer(conf)
 
+    task = Task({"type": "sample", "stage": "recognized", "platform": "win32"})
+
     with open(args.sample, "rb") as f:
         sample = Resource("sample", f.read())
-
-    task = Task({"type": "sample", "stage": "recognized", "platform": "win32"})
     task.add_resource("sample", sample)
+
+    # Add filename
+    filename = os.path.basename(args.sample)
+    task.add_payload("file_name", os.path.splitext(filename)[0])
+
+    # Extract and add extension
+    extension = os.path.splitext(filename)[1][1:]
+    if extension:
+        task.headers['extension'] = extension
+
+    if args.start_command is not None:
+        task.add_payload("start_command", args.start_command)
 
     producer.send_task(task)
 
