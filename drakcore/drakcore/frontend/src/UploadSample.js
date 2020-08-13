88621d1ea02cd1c2dbc586fd48988b85109d7a6e
Konstanty Cieslinski <konstanty.cieslinski@cert.pl>
2020-08-13 15:40:08 +0200

    fix format

--- drakcore/drakcore/frontend/src/UploadSample.js
+++ drakcore/drakcore/frontend/src/UploadSample.js
@@ -100,12 +100,19 @@ class UploadSample extends Component {
     const hasCustomFileName = this.state.customFileName !== "";
     const hasCustomStartCmd = this.state.customStartCmd !== "";
 
-    const fname = hasCustomFileName ? this.state.customFileName : this.state.file.name;
+    const fname = hasCustomFileName
+      ? this.state.customFileName
+      : this.state.file.name;
     // Check for whitespace
     if (!fname.match(/^\S+$/)) {
       errMsg = "File name contains invalid characters";
-    } else if (!fname.match(/^[\w]+\.(?:dll|exe|doc|docm|docx|dotm|xls|xlsx|xlsm|xltx|xltm)$/i)) {
-      errMsg = "Invalid file name. Only .dll, .exe and office files are supported";
+    } else if (
+      !fname.match(
+        /^[\w]+\.(?:dll|exe|doc|docm|docx|dotm|xls|xlsx|xlsm|xltx|xltm)$/i
+      )
+    ) {
+      errMsg =
+        "Invalid file name. Only .dll, .exe and office files are supported";
     }
 
     if (hasCustomStartCmd) {
