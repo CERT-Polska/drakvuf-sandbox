f13126267603574dc6d8d2af21d12e1eab85088a
Hubert Jasudowicz <hubert.jasudowicz@cert.pl>
2020-08-31 19:48:35 +0200

    Rework apicall component to use react-window

--- drakcore/drakcore/frontend/src/AnalysisApicall.js
+++ drakcore/drakcore/frontend/src/AnalysisApicall.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Component } from "react";
+import { FixedSizeList as List } from "react-window";
+import OptionPicker from "./OptionPicker";
 import "./App.css";
 import api from "./api";
-import OptionPicker from "./OptionPicker";
 
 class AnalysisApicall extends Component {
   constructor(props) {
@@ -13,9 +14,13 @@ class AnalysisApicall extends Component {
     this.state = {
       calls: null,
       processList: [],
+      filter: '',
+      filteredResults: [],
     };
 
     this.pidChanged = this.pidChanged.bind(this);
+    this.filterChanged = this.filterChanged.bind(this);
+    this.computeFiltered = this.computeFiltered.bind(this);
   }
 
   async pidChanged(new_pid) {
@@ -24,9 +29,31 @@ class AnalysisApicall extends Component {
       const res = await api.getApiCalls(this.analysis_id, new_pid);
       const calls = res.data.split("\n").map(JSON.parse);
       this.setState({ calls });
+      this.computeFiltered(this.state.filter);
     } catch (e) {
       this.setState({ calls: [] });
     }
+  }
+
+  computeFiltered(filter) {
+    if (filter === '') {
+      this.setState({ filteredResults: this.state.calls });
+      return;
+    }
+    try {
+      let regex = new RegExp(filter, 'gi');
+      this.setState({
+        filteredResults: this.state.calls.filter((elem) =>
+          regex.test(elem.method)
+        ),
+      });
+    } catch  { }
+  }
+
+  filterChanged(event) {
+    const newFilter = event.target.value;
+    this.setState({filter: newFilter});
+    this.computeFiltered(newFilter);
   }
 
   async componentDidMount() {
@@ -37,14 +64,11 @@ class AnalysisApicall extends Component {
       let result = [];
 
       process_tree.forEach((proc) => {
-        result.push({
-          key: proc.pid,
-          value: `${proc.pid} – ${proc.procname || "unnamed process"}`,
-        });
+        result.push({ key: proc.pid, value: `${proc.pid} – ${proc.procname || "unnamed process"}` });
         result.push(...treeFlatten(proc.children));
       });
 
-      result.sort((a, b) => a.pid - b.pid);
+      result.sort((a, b) => a.key - b.key);
 
       return result;
     }
@@ -82,33 +106,62 @@ class AnalysisApicall extends Component {
         </div>
       );
     } else {
-      let tableContent = this.state.calls.map((entry, i) => (
-        <tr key={i}>
-          <td>{entry.timestamp}</td>
-          <td>
-            <code>{entry.method}</code>
-          </td>
-          <td>
-            {entry.arguments.map((arg, i) => (
-              <div key={i} className="badge-outline-primary badge mr-1">
-                {arg}
-              </div>
-            ))}
-          </td>
-        </tr>
-      ));
-      content = (
-        <table className="table table-centered apicallTable">
-          <thead>
-            <tr>
-              <th>Timestamp</th>
-              <th>Method</th>
-              <th>Arguments</th>
-            </tr>
-          </thead>
-          <tbody>{tableContent}</tbody>
-        </table>
+      // let tableContent = this.state.calls.map((entry, i) => (
+      //   <tr key={i}>
+      //     <td>{entry.timestamp}</td>
+      //     <td>
+      //       <code>{entry.method}</code>
+      //     </td>
+      //     <td>
+      //       {entry.arguments.map((arg, i) => (
+      //         <div key={i} className="badge-outline-primary badge mr-1">
+      //           {arg}
+      //         </div>
+      //       ))}
+      //     </td>
+      //   </tr>
+      // ));
+
+      const Row = ({ data, index, style }) => {
+        const entry = data[index];
+        const args = entry.arguments.join(", ");
+        return (
+          <div style={style} className="d-flex flex-row align-content-stretch">
+            <div className="p-1 d-none d-md-block">{entry.timestamp}</div>
+            <div className="p-1" style={{overflow: "hidden", wordBreak:"break-all"}}>
+              <code>
+                {entry.method}({args}) = ?
+              </code>
+            </div>
+          </div>
+        );
+      };
+
+      let tableContent = (
+        <List
+          itemData={this.state.filteredResults}
+          height={600}
+          itemCount={this.state.filteredResults ? this.state.filteredResults.length : 0}
+          itemSize={28}
+        >
+          {Row}
+        </List>
       );
+
+      content = tableContent;
+
+      // content = (
+      //   <table className="table table-centered apicallTable">
+      //     <thead>
+      //       <tr>
+      //         <th>Timestamp</th>
+      //         <th>Method</th>
+      //         <th>Arguments</th>
+      //       </tr>
+      //     </thead>
+      //     <tbody>{tableContent}</tbody>
+      //   </table>
+      // );
     }
 
     return (
@@ -119,12 +172,22 @@ class AnalysisApicall extends Component {
 
         <div className="card tilebox-one">
           <div className="card-body">
-            <OptionPicker
-              defaultSelection={url_pid}
-              data={this.state.processList}
-              onChange={this.pidChanged}
-              className="mb-1"
-            />
+            <div className="row mb-2">
+              <div className="col-9">
+                <OptionPicker
+                  defaultSelection={url_pid}
+                  data={this.state.processList}
+                  onChange={(pid) => this.pidChanged(parseInt(pid))}
+                />
+              </div>
+              <input
+                type="text"
+                className="form-control col-3"
+                placeholder="Search API calls..."
+                value={this.state.filter}
+                onChange={this.filterChanged}
+              />
+            </div>
             {content}
           </div>
         </div>
