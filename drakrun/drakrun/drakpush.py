9e0e6cf8fedeb9b77c37c5aee39acbabd872e5c8
Michał Leszczyński <michal.leszczynski@cert.pl>
2020-09-17 12:57:29 +0200

    Update drakpush.py

--- drakrun/drakrun/drakpush.py
+++ drakrun/drakrun/drakpush.py
@@ -9,7 +9,7 @@ def main():
     parser = argparse.ArgumentParser(description='Push sample to the karton')
     parser.add_argument('sample', help='Path to the sample')
     parser.add_argument('--start_command', help='e.g. start %f, %f will be replaced by file name', required=False)
-    parser.add_argument('--timeout', type=int, default=600, type=int, help='analysis timeout in seconds', required=False)
+    parser.add_argument('--timeout', default=600, type=int, help='analysis timeout in seconds', required=False)
     args = parser.parse_args()
 
     conf = Config(os.path.join(ETC_DIR, 'config.ini'))
