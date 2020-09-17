f94e9096d9fbe8c6c1d607dbb5c89a71962b28a8
Konstanty Cieslinski <konstanty.cieslinski@cert.pl>
2020-09-17 14:55:22 +0200

    Bump to 0.9.0

--- drakrun/setup.py
+++ drakrun/setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name="drakrun",
-    version="0.8.1",
+    version="0.9.0",
     description="DRAKRUN",
     package_dir={"drakrun": "drakrun"},
     packages=["drakrun"],
