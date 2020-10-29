017f317ba564fab01068cd2ac67d62bcb1351ad8
Michał Leszczyński <michal.leszczynski@cert.pl>
2020-10-29 11:47:06 +0100

    Sanity check whether required Xen boot options are set (#332)

--- drakrun/drakrun/util.py
+++ drakrun/drakrun/util.py
@@ -35,3 +35,35 @@ def patch_config(cfg):
 def get_domid_from_instance_id(instance_id: str) -> int:
     output = subprocess.check_output(["xl", "domid", f"vm-{instance_id}"])
     return int(output.decode('utf-8').strip())
+
+
+def get_xl_info():
+    xl_info_out = subprocess.check_output(['xl', 'info']).decode('utf-8', 'replace')
+    xl_info_lines = xl_info_out.strip().split('\n')
+
+    cfg = {}
+
+    for line in xl_info_lines:
+        k, v = line.split(':', 1)
+        k, v = k.strip(), v.strip()
+        cfg[k] = v
+
+    return cfg
+
+
+def get_xen_commandline(parsed_xl_info):
+    opts = parsed_xl_info['xen_commandline'].split(' ')
+
+    cfg = {}
+
+    for opt in opts:
+        if not opt.strip():
+            continue
+
+        if '=' not in opt:
+            cfg[opt] = '1'
+        else:
+            k, v = opt.split('=', 1)
+            cfg[k] = v
+
+    return cfg
