99f0311983494b1878c1265408725c44baa7dcfa
Hubert Jasudowicz <hubert.jasudowicz@cert.pl>
2020-11-03 13:56:33 +0100

    drakcore: Load analysis list from cache
    Instead of asking MinIO every time someone requests a list of metadata,
    load them directly from the local database. This significantly speeds up
    the requests, however introduces additional obstacle - how to populate
    the local cache. Solve this by abusing the postprocess service - trigger a
    request for metadata on a newly received analysis.

--- drakcore/drakcore/database.py
+++ drakcore/drakcore/database.py
@@ -1,7 +1,7 @@
 import json
 from msql import BaseDb
 from pathlib import Path
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List, Tuple
 
 AnalysisMetadata = Dict[str, Any]
 
@@ -18,3 +18,15 @@ class Database(BaseDb):
         with self.get_cursor() as cursor:
             cursor.execute("INSERT INTO metadata (uid, value) VALUES (?, ?)",
                            (analysis_uid, json.dumps(metadata)))
+
+    def get_latest_metadata(self, limit: int = 100, offset: int = 0) -> Dict[str, Any]:
+        def into_result(row):
+            return dict(id=row["uid"], meta=json.loads(row["value"]))
+        with self.get_cursor() as cursor:
+            cursor.execute(("SELECT uid, value "
+                            "FROM metadata "
+                            "ORDER BY json_extract(value, '$.time_finished') DESC "
+                            "LIMIT ? "
+                            "OFFSET ?"), (limit, offset))
+            result = cursor.fetchall()
+            return map(into_result, result)
