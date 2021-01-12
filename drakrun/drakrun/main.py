b163038f50a1e50df3f86432257e926da51ab83e
Hubert Jasudowicz <hubert.jasudowicz@cert.pl>
2021-01-12 17:28:00 +0100

    drakrun: Plug drakrun logger into karton logging (#409)
    This allows logging from components other than karton service itself

--- drakrun/drakrun/main.py
+++ drakrun/drakrun/main.py
@@ -108,6 +108,13 @@ class DrakrunKarton(Karton):
 
     def __init__(self, config: Config, instance_id: int):
         super().__init__(config)
+
+        # Now that karton is set up we can plug in our logger
+        logger = logging.getLogger("drakrun")
+        for handler in self.log.handlers:
+            logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+
         self.instance_id = instance_id
         self.install_info = InstallInfo.load()
         self.default_timeout = int(self.config.config['drakrun'].get('analysis_timeout') or 60 * 10)
