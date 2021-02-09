3ff8ff50b7af1464a974656cc0aad137f1b84113
Hubert Jasudowicz <hubert.jasudowicz@cert.pl>
2021-02-09 13:49:57 +0100

    docs: Add development guide

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,10 @@ latex_engine = 'xelatex'
 extensions = [
     "sphinx_rtd_theme",
     "IPython.sphinxext.ipython_console_highlighting",
+    "sphinx.ext.graphviz",
 ]
+
+graphviz_output_format = 'svg'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
