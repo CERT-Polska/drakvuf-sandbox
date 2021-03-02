359ed4ce4f6658e8829ebf93a70d3dc4013e0c19
Michał Leszczyński <michal.leszczynski@cert.pl>
2021-03-02 23:51:02 +0100

    Update test-altp2m.c

--- drakrun/drakrun/tools/test-altp2m.c
+++ drakrun/drakrun/tools/test-altp2m.c
@@ -1,29 +1,3 @@
-/* The LibVMI Library is an introspection library that simplifies access to
- * memory in a target virtual machine or in a file containing a dump of
- * a system's physical memory.  LibVMI is based on the XenAccess Library.
- *
- * Copyright 2011 Sandia Corporation. Under the terms of Contract
- * DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government
- * retains certain rights in this software.
- *
- * Author: Bryan D. Payne (bdpayne@acm.org)
- *
- * This file is part of LibVMI.
- *
- * LibVMI is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * LibVMI is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with LibVMI.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
@@ -38,7 +12,7 @@ int main (int argc, char **argv)
 {
     vmi_instance_t vmi;
 
-    /* this is the VM or file that we are looking at */
+    /* this is the VM that we are looking at */
     if (argc != 2) {
         printf("Usage: %s <vmname>\n", argv[0]);
         return 1;
