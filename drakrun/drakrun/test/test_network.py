0c62bd12ecfa35a8a2e11de9083249583ade39f9
manorit chawdhry <manorit2001@gmail.com>
2021-03-10 16:26:56 +0530

    improved tests

--- drakrun/drakrun/test/test_network.py
+++ drakrun/drakrun/test/test_network.py
@@ -12,12 +12,23 @@ import os
 import subprocess
 
 
+def tool_exists(tool):
+    try:
+        subprocess.check_output(f'which {tool}', shell=True)
+    except subprocess.CalledProcessError as e:
+        return False
+    else:
+        return True
+
+
+@pytest.mark.skipif(not tool_exists('brctl'), reason="brctl does not exist")
 def test_network_setup():
     setup_vm_network(1, True, find_default_interface(), '8.8.8.8')
-
     assert iptable_rule_exists("INPUT -i drak1 -p udp --dport 67:68 --sport 67:68 -j ACCEPT") is True
 
 
+@pytest.mark.skipif(not tool_exists('dnsmasq'), reason="dnsmasq does not exist")
+@pytest.mark.skipif(not tool_exists('brctl'), reason="brctl does not exist")
 def test_dnsmasq_start():
     start_dnsmasq(1, '8.8.8.8', True)
     try:
@@ -26,6 +37,8 @@ def test_dnsmasq_start():
         assert e.returncode == 0
 
 
+@pytest.mark.skipif(not tool_exists('dnsmasq'), reason="dnsmasq does not exist")
+@pytest.mark.skipif(not tool_exists('brctl'), reason="brctl does not exist")
 def test_dnsmasq_stop():
     stop_dnsmasq(1)
     try:
@@ -34,6 +47,7 @@ def test_dnsmasq_stop():
         assert e.returncode == 1
 
 
+@pytest.mark.skipif(not tool_exists('brctl'), reason="brctl does not exist")
 def test_network_delete():
     delete_vm_network(1, True, find_default_interface(), '8.8.8.8')
     assert iptable_rule_exists("INPUT -i drak1 -p udp --dport 67:68 --sport 67:68 -j ACCEPT") is False
