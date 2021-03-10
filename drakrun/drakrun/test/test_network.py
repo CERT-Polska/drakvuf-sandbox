31eacbda187936f3e8ac7d76e728148a900fc04e
manorit chawdhry <manorit2001@gmail.com>
2021-03-10 15:48:39 +0530

    lint fix

--- drakrun/drakrun/test/test_network.py
+++ drakrun/drakrun/test/test_network.py
@@ -11,10 +11,12 @@ from drakrun.draksetup import find_default_interface
 import os
 import subprocess
 
+
 def test_network_setup():
     setup_vm_network(1, True, find_default_interface(), '8.8.8.8')
 
     assert iptable_rule_exists("INPUT -i drak1 -p udp --dport 67:68 --sport 67:68 -j ACCEPT") is True
+
 
 def test_dnsmasq_start():
     start_dnsmasq(1, '8.8.8.8', True)
@@ -23,12 +25,14 @@ def test_dnsmasq_start():
     except subprocess.CalledProcessError as e:
         assert e.returncode == 0
 
+
 def test_dnsmasq_stop():
     stop_dnsmasq(1)
     try:
         subprocess.check_output('pgrep dnsmasq', shell=True)
     except subprocess.CalledProcessError as e:
         assert e.returncode == 1
+
 
 def test_network_delete():
     delete_vm_network(1, True, find_default_interface(), '8.8.8.8')
