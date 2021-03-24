eee5afe71b61163156db1a27987d815c56d5bf6e
manorit chawdhry <manorit2001@gmail.com>
2021-03-24 23:36:08 +0530

    helper for unit testings

---
+++ drakrun/drakrun/test/common_utils.py
@@ -0,0 +1,28 @@
+import shutil
+import os
+import contextlib
+import subprocess
+
+
+def tool_exists(tool):
+    return subprocess.run(["which", tool]).returncode == 0
+
+
+@contextlib.contextmanager
+def remove_files(paths):
+
+    # change names
+    for i in paths:
+        try:
+            shutil.move(i, f"{i}.bak")
+        except FileNotFoundError:
+            pass
+
+    yield
+
+    # restore the names
+    for i in paths:
+        try:
+            shutil.move(f"{i}.bak", i)
+        except FileNotFoundError:
+            pass
