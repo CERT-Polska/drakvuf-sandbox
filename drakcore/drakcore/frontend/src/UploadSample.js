45d970b30743c77f40c565a9c716777e691ad24a
Hubert Jasudowicz <hubert.jasudowicz@cert.pl>
2021-04-19 15:11:12 +0200

    reformat

--- drakcore/drakcore/frontend/src/UploadSample.js
+++ drakcore/drakcore/frontend/src/UploadSample.js
@@ -215,8 +215,15 @@ class UploadSample extends Component {
   }
 
   displayPluginImplications(enabledPlugins) {
-    if (enabledPlugins.indexOf("ipt") !== -1 && enabledPlugins.indexOf("codemon") == -1) {
-      return <p class="text-muted">Using <code>ipt</code> plugin implies using <code>codemon</code>.</p>;
+    if (
+      enabledPlugins.indexOf("ipt") !== -1 &&
+      enabledPlugins.indexOf("codemon") == -1
+    ) {
+      return (
+        <p class="text-muted">
+          Using <code>ipt</code> plugin implies using <code>codemon</code>.
+        </p>
+      );
     }
 
     return <span />;
@@ -290,7 +297,7 @@ class UploadSample extends Component {
               onSelect={this.handlePluginsChange}
               onRemove={this.handlePluginsChange}
             />
-	    {this.displayPluginImplications(this.state.enabledPlugins)}
+            {this.displayPluginImplications(this.state.enabledPlugins)}
           </div>
           <div className="collapse" id="customOptions">
             <OptionalField
