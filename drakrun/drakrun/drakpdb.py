dc7be7be495636ee3b62b338976ab0abaa2d585e
Arkadiusz Wróbel <29861114+catsuryuu@users.noreply.github.com>
2022-04-08 14:20:18 +0200

    Fix DLL file lists (#710)

--- drakrun/drakrun/drakpdb.py
+++ drakrun/drakrun/drakpdb.py
@@ -15,8 +15,9 @@ from tqdm import tqdm
 DLL = NamedTuple("DLL", [("path", str), ("dest", str), ("arg", Optional[str])])
 
 
+# profile file list, without 'C:\' and with '/' instead of '\'
 # something is wrong if these DLLs fail
-compulsory_dll_file_list = [
+required_dll_file_list = [
     DLL("Windows/System32/ntdll.dll", "amd64_ntdll_profile", "--json-ntdll"),
     DLL("Windows/SysWOW64/ntdll.dll", "wow64_ntdll_profile", "--json-wow"),
     DLL("Windows/System32/win32k.sys", "amd64_win32k_profile", "--json-win32k"),
@@ -29,7 +30,7 @@ compulsory_dll_file_list = [
 ]
 
 # profile file list, without 'C:\' and with '/' instead of '\'
-dll_file_list = [
+unrequired_dll_file_list = [
     DLL("Windows/System32/drivers/tcpip.sys", "amd64_tcpip_profile", "--json-tcpip"),
     DLL("Windows/System32/sspicli.dll", "amd64_sspicli_profile", "--json-sspicli"),
     DLL(
@@ -128,6 +129,9 @@ dll_file_list = [
     DLL("Windows/System32/wtsapi32.dll", "amd64_wtsapi32_profile", None),
     DLL("Windows/SysWOW64/wtsapi32.dll", "x86_wtsapi32_profile", None),
 ]
+
+
+dll_file_list = required_dll_file_list + unrequired_dll_file_list
 
 
 CV_RSDS_HEADER = "CV_RSDS" / Struct(
