f9bd28b3e729ba63ccf80134fdff8424264e983a
Arkadiusz Wróbel <catsuryuu@cert.pl>
2022-04-12 09:21:29 +0200

    Fix release debs for Bullseye

--- drakcore/package/find-python.sh
+++ drakcore/package/find-python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PYTHON_BIN=$(which python3.8 || which python3.7)
+PYTHON_BIN=$(which python3.9 || which python3.8 || which python3.7)
 PYTHON_PACK=$(basename "$PYTHON_BIN")
 
 sed "s/{{PYTHON_PACKAGES}}/${PYTHON_PACK}, lib${PYTHON_PACK}/" package/control.template > debian/control
