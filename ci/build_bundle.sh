95f87f30017dad12bc2078cdda459c5f76389214
Bonus <root@bonusplay.pl>
2023-01-17 16:43:16 +0100

    debugging CI

--- ci/build_bundle.sh
+++ ci/build_bundle.sh
@@ -42,10 +42,17 @@ popd
 # Package DRAKVUF
 pushd drakvuf
 mkdir /out
+
 # remove volatility3
 sed -i '/volatility3/d' ./package/mkdeb
 # change drakvuf build dir
 sed -i 's/\/build/\/build\/drakvuf/g' ./package/mkdeb
+
+# DEBUGGING
+find dwarf2json
+cat ./package/mkdeb
+pwd
+ls -lha
 
 sh ./package/mkdeb
 popd
