7efd5b20e2ed11b7b05ddf51417a5e4aac5fef8b
Paweł Srokosz <pawel.srokosz@cert.pl>
2023-08-07 20:39:36 +0200

    CI: Fix liveness check

--- test/vm-runner-client/vm_runner_client/client.py
+++ test/vm-runner-client/vm_runner_client/client.py
@@ -10,6 +10,7 @@ from fabric import Connection
 from .sockstls import create_connection
 from .sockstls_requests import make_session
 from python_socks import ProxyError
+from python_socks._proto.socks5 import ReplyCode
 
 
 class VMRunnerConfig:
@@ -75,14 +76,26 @@ class DrakvufVM:
         })
         response.raise_for_status()
 
-    def wait_alive(self):
+    def is_alive(self):
+        try:
+            self.connect_tcp(22).close()
+            return True
+        except ProxyError as e:
+            if e.error_code in [
+                ReplyCode.CONNECTION_REFUSED,
+                ReplyCode.HOST_UNREACHABLE
+            ]:
+                return False
+            else:
+                raise
+
+    def wait_for_state(self, alive: bool):
         for tries in range(30):
-            try:
-                self.connect_tcp(22).close()
-                return True
-            except (OSError, ProxyError) as e:
-                logging.info(f"Try [{tries+1}/30]: {str(e)}")
-                time.sleep(1)
+            if self.is_alive() == alive:
+                return
+            logging.info(f"Try {tries}/30: Machine still {'not ' if alive else ''}alive")
+            time.sleep(1)
+        raise RuntimeError("Machine not reached in expected time")
 
     @staticmethod
     def get_vm_identity():
