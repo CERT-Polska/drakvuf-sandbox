0201b5696c3d5905ca7913b0f4a78d5b3c14094e
Paweł Srokosz <pawel.srokosz@cert.pl>
2023-08-10 13:42:59 +0200

    Well, we actually need that libpython

--- drakrun/package/find-python.sh
+++ drakrun/package/find-python.sh
@@ -4,5 +4,5 @@ PYTHON_BIN_LINK=$(which python3)
 PYTHON_BIN=$(readlink -f "$PYTHON_BIN_LINK")
 PYTHON_PACK=$(basename "$PYTHON_BIN")
 
-sed "s/{{PYTHON_PACKAGES}}/${PYTHON_PACK}/" package/control.template > debian/control
+sed "s/{{PYTHON_PACKAGES}}/${PYTHON_PACK}, lib${PYTHON_PACK}/" package/control.template > debian/control
 echo -n "$PYTHON_BIN" > package/python-bin
