f4381edc00052c2c4926195051f3944a06719bf8
Paweł Srokosz <pawel.srokosz@cert.pl>
2023-09-25 19:32:25 +0200

    Connect postinstall

--- draklib/draklib/draksh/__init__.py
+++ draklib/draklib/draksh/__init__.py
@@ -3,5 +3,7 @@ Drakvuf-Sandbox shell utility for easy managing and debugging Drakvuf instance
 """
 from .install import install
 from .main import main
+from .postinstall import postinstall
 
 main.add_command(install)
+main.add_command(postinstall)
