96f5003ef93a0b6b4607e1184161591b8be2bd03
Paweł Srokosz <pawel.srokosz@cert.pl>
2023-09-26 18:50:18 +0200

    Forgot to register it

--- draklib/draklib/draksh/__init__.py
+++ draklib/draklib/draksh/__init__.py
@@ -4,6 +4,9 @@ Drakvuf-Sandbox shell utility for easy managing and debugging Drakvuf instance
 from .install import install
 from .main import main
 from .postinstall import postinstall
+from .run import run
 
 main.add_command(install)
 main.add_command(postinstall)
+main.add_command(run)
+
