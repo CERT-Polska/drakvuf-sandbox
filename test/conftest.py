f02d04267615ac93b1915403d17f3d657558ac52
msm <msm@cert.pl>
2024-01-25 18:35:00 +0100

    Listen to your heart

--- test/conftest.py
+++ test/conftest.py
@@ -113,7 +113,7 @@ def drakmon_setup():
 
     with drakvuf_vm.connect_ssh() as ssh:
         ssh.run("apt-get --allow-releaseinfo-change update", in_stream=False)
-        apt_install(ssh, ["redis-server", "python3", "python3-pip", "git", "dnsmasq"])
+        apt_install(ssh, ["redis-server", "python3", "python3-pip", "git", "dnsmasq", "tcpdump", "genisoimage", "qemu-utils", "iptables"])
         pip_install(ssh, ["pip"], "--upgrade")
         for d in drakvuf_sandbox_debs:
             if str(d).endswith(".deb"):
