a0785e81f62c0a2dabd6a3c25cb53355104bd13d
Paweł Srokosz <pawel.srokosz@cert.pl>
2024-04-02 11:06:11 +0200

    Apply suggestion from code review

--- drakrun/drakrun/lib/paths.py
+++ drakrun/drakrun/lib/paths.py
@@ -1,9 +1,9 @@
 import os
 
-ETC_DIR = os.getenv("DRAKRUN_ETC_DIR") or "/etc/drakrun"
+ETC_DIR = "/etc/drakrun"
 VM_CONFIG_DIR = os.path.join(ETC_DIR, "configs")
 
-LIB_DIR = os.getenv("DRAKRUN_LIB_DIR") or "/var/lib/drakrun"
+LIB_DIR = "/var/lib/drakrun"
 PROFILE_DIR = os.path.join(LIB_DIR, "profiles")
 RUNTIME_FILE = os.path.join(PROFILE_DIR, "runtime.json")
 APISCOUT_PROFILE_DIR = os.path.join(LIB_DIR, "apiscout_profile")
