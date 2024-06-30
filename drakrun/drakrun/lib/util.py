67728caa2dcb1604d267e109e6809f043b9c47f8
Paweł Srokosz <pawel.srokosz@cert.pl>
2024-06-30 17:20:01 +0200

    Refactor: remove unused try_run function (#911)

--- drakrun/drakrun/lib/util.py
+++ drakrun/drakrun/lib/util.py
@@ -4,7 +4,6 @@ import logging
 import os
 import re
 import subprocess
-import traceback
 from dataclasses import dataclass, field
 
 from dataclasses_json import config, dataclass_json
@@ -71,65 +70,6 @@ def safe_delete(file_path) -> bool:
     except OSError as e:
         log.warning(f"{e.filename}: {e.strerror}")
         return False
-
-
-def try_run(
-    list_args: list, msg: str, reraise=True, **kwargs
-) -> subprocess.CompletedProcess:
-    """
-    Runs subprocess.run in a try except with some default arguments ( which can be overriden by supplying kwargs )
-
-            Parameters:
-                    list_args (list): Subprocess list which will be passed to subprocess.run
-                    msg (str): A meaningful error message to be raised during non-zero exit code
-                    reraise (bool):
-                        True: will raise an Exception with traceback on error
-                        False: will log a warning on error
-                    **kwargs: additional parameters to be passed to subprocess.run
-
-            Defaults:
-                    subprocess.run is called with the following arguments by default
-                    stderr = subprocess.PIPE
-                    stdout = subprocess.PIPE # to print to console, pass kwargs ( stdout = None )
-                    check = True
-
-            Returns:
-                    sub (subprocess.CompletedProcess): Object with the completed process
-                        or
-                    None: if the subprocess failed and reraise=False
-    """
-
-    try:
-        kwargs["stdout"]
-    except KeyError:
-        kwargs["stdout"] = subprocess.PIPE
-
-    if kwargs.get("stderr") is None:
-        kwargs["stderr"] = subprocess.PIPE
-    if kwargs.get("check") is None:
-        kwargs["check"] = True
-
-    try:
-        sub = subprocess.run(list_args, **kwargs)
-    except (FileNotFoundError, TypeError) as e:
-        log.debug("arguments to subprocess")
-        log.debug(list_args)
-        log.debug(msg)
-        log.debug(kwargs)
-        raise Exception("Command not found") from e
-    except subprocess.CalledProcessError as e:
-        if e.stdout is not None:
-            log.debug("stdout: \n{}".format(e.stdout.decode()))
-        if e.stderr is not None:
-            log.debug("stderr: \n{}".format(e.stderr.decode()))
-        log.debug("returncode: {}".format(e.returncode))
-        if reraise:
-            raise Exception(msg) from e
-        else:
-            log.warning(msg)
-            log.debug(traceback.format_exc())
-            return None
-    return sub
 
 
 @contextlib.contextmanager
