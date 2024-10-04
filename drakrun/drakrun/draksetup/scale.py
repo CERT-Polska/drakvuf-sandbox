8dc3e53688346f732b1db2dbfa73f177d59dc682
Paweł Srokosz <pawel.srokosz@cert.pl>
2024-10-04 17:18:50 +0200

    Fix: wrong condition in draksetup scale

--- drakrun/drakrun/draksetup/scale.py
+++ drakrun/drakrun/draksetup/scale.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 @click.argument("scale_count", type=int)
 def scale(scale_count):
     """Enable or disable additional parallel instances of drakrun service.."""
-    if scale_count >= 0:
+    if scale_count < 0:
         raise RuntimeError(
             "Invalid value of scale parameter - must be a positive number."
         )
