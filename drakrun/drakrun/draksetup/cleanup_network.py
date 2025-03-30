e991d9985b92707c825b50a2a77cf97377712b90
psrok1 <pawel.srokosz@cert.pl>
2025-03-30 23:25:12 +0200

    draksetup: remove additional commands

--- drakrun/drakrun/draksetup/cleanup_network.py
+++
@@ -1,9 +0,0 @@
-import click
-
-from drakrun.lib.networking import delete_all_vm_networks, delete_legacy_iptables
-
-
-@click.command(help="Cleanup changes in iptables and bridges")
-def cleanup_network():
-    delete_legacy_iptables()
-    delete_all_vm_networks()
--- drakrun/drakrun/draksetup/main.py
+++ drakrun/drakrun/draksetup/main.py
@@ -3,15 +3,10 @@ import os
 
 import click
 
-from .cleanup import cleanup
-from .cleanup_network import cleanup_network
 from .install import install
-from .install_minio import install_minio
 from .modify_vm0 import modify_vm0
 from .mount import mount
 from .postinstall import postinstall
-from .scale import scale
-from .snapshot import snapshot
 from .vm_start import vm_start
 from .vm_stop import vm_stop
 
@@ -33,9 +28,4 @@ main.add_command(postinstall)
 main.add_command(vm_start)
 main.add_command(vm_stop)
 main.add_command(mount)
-main.add_command(scale)
-main.add_command(snapshot)
-main.add_command(cleanup)
-main.add_command(cleanup_network)
-main.add_command(install_minio)
 main.add_command(modify_vm0)
