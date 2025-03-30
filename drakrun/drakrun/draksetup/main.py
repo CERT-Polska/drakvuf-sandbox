b93fb28301d98334ad5abee1131bc72f15dde2bc
psrok1 <pawel.srokosz@cert.pl>
2025-03-30 23:09:08 +0200

    draksetup: remove memdump command

--- drakrun/drakrun/draksetup/main.py
+++ drakrun/drakrun/draksetup/main.py
@@ -7,7 +7,6 @@ from .cleanup import cleanup
 from .cleanup_network import cleanup_network
 from .install import install
 from .install_minio import install_minio
-from .memdump import memdump
 from .modify_vm0 import modify_vm0
 from .mount import mount
 from .postinstall import postinstall
@@ -32,7 +31,6 @@ main.add_command(postinstall)
 main.add_command(mount)
 main.add_command(scale)
 main.add_command(snapshot)
-main.add_command(memdump)
 main.add_command(cleanup)
 main.add_command(cleanup_network)
 main.add_command(install_minio)
