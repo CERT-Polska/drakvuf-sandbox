CERT-Polska/drakvuf-sandbox

commit 976799bc7708254f8030276663a4e0374319c9a2

Rewire capa_plugin configuration with main configuration object (#1054)
* Rewire capa_plugin configuration with main configuration object

* Change section name

drakrun/analyzer/postprocessing/plugins/capa_plugin/capa_processor.py
@@ -26,7 +26,7 @@ from capa.main import (
 from capa.render.result_document import MatchResults as ResultDocumentMatchResults
 from capa.rules import Rule, RuleSet, get_rules, get_rules_and_dependencies
 
-from .capa_config import CapaConfigSection
+from drakrun.lib.config import load_config
 
 logger = logging.getLogger(__name__)
 
@@ -296,9 +296,7 @@ def construct_ttp_blocks(
 
 
 def capa_analysis(analysis_dir: Path) -> None:
-    # todo: this should be filled with configuration
-    # right now only defaults are supported
-    config = CapaConfigSection()
+    config = load_config().capa_rules
 
     # capa rules directory
     capa_rules_dir = config.rules_directory

drakrun/analyzer/postprocessing/plugins/capa_plugin/capa_config.py
@@ -1,13 +1,0 @@
-from pathlib import Path
-
-from pydantic import BaseModel
-
-from drakrun.lib.paths import PACKAGE_DIR
-
-
-class CapaConfigSection(BaseModel):
-    rules_directory: Path = PACKAGE_DIR / "data" / "capa-rules"
-    analyze_drakmon_log: bool = True
-    analyze_memdumps: bool = False
-    analyze_only_malware_pids: bool = False
-    worker_pool_processes: int = 4

drakrun/lib/config.py
@@ -4,7 +4,7 @@ from typing import Any, Dict, List, Optional
 import tomli
 from pydantic import BaseModel, ConfigDict
 
-from drakrun.lib.paths import CONFIG_PATH
+from drakrun.lib.paths import CONFIG_PATH, PACKAGE_DIR
 
 DNS_USE_GATEWAY_ADDRESS = "use-gateway-address"
 OUT_INTERFACE_DEFAULT = "default"
@@ -43,11 +43,20 @@ class DrakrunConfigSection(BaseModel):
     extra_output_subdirs: Optional[List[str]] = None
 
 
+class CapaConfigSection(BaseModel):
+    rules_directory: pathlib.Path = PACKAGE_DIR / "data" / "capa-rules"
+    analyze_drakmon_log: bool = True
+    analyze_memdumps: bool = False
+    analyze_only_malware_pids: bool = False
+    worker_pool_processes: int = 4
+
+
 class DrakrunConfig(BaseModel):
     model_config = ConfigDict(extra="ignore")
     redis: RedisConfigSection
     network: NetworkConfigSection
     drakrun: DrakrunConfigSection
+    capa: CapaConfigSection
 
     @staticmethod
     def load(filename: str) -> "DrakrunConfig":

commit 3761256db959ac6a04c523ef49186e02d1fcc450

Fix: configuration bug introduced in #1054

drakrun/analyzer/postprocessing/plugins/capa_plugin/capa_processor.py
@@ -296,7 +296,7 @@ def construct_ttp_blocks(
 
 
 def capa_analysis(analysis_dir: Path) -> None:
-    config = load_config().capa_rules
+    config = load_config().capa
 
     # capa rules directory
     capa_rules_dir = config.rules_directory