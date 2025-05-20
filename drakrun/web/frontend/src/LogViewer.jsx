377800b76f44bb3ba7f83cc172ab10014214b547
psrok1 <pawel.srokosz@cert.pl>
2025-05-20 15:49:38 +0200

    Fix lazy loading in LogViewer

--- drakrun/web/frontend/src/LogViewer.jsx
+++ drakrun/web/frontend/src/LogViewer.jsx
@@ -1,5 +1,5 @@
 import { getLog } from "./api.js";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { LazyLog } from "@melloware/react-logviewer";
 
 const DEFAULT_CHUNK_SIZE = 300 * 1024;
@@ -11,17 +11,22 @@ async function* getLogLoader({
 }) {
     let currentStart = 0;
     while (1) {
-        const data = await getLog({
-            analysisId,
-            logType,
-            rangeStart: currentStart,
-            rangeEnd: currentStart + (chunkSize - 1),
-        });
-        const dataLength = data.lastIndexOf("\n") + 1;
-        yield data.slice(0, dataLength);
-        currentStart += dataLength;
-        if (data.length < DEFAULT_CHUNK_SIZE) {
-            break;
+        try {
+            const data = await getLog({
+                analysisId,
+                logType,
+                rangeStart: currentStart,
+                rangeEnd: currentStart + (chunkSize - 1),
+            });
+            const dataLength = data.lastIndexOf("\n") + 1;
+            yield data.slice(0, dataLength);
+            currentStart += dataLength;
+        } catch (err) {
+            if (err.status === 416) {
+                break;
+            } else {
+                throw err;
+            }
         }
     }
 }
@@ -34,29 +39,26 @@ export function LogViewer({
 }) {
     const [loading, setLoading] = useState(true);
     const [content, setContent] = useState("");
-    const logLoader = useRef(null);
+    const logLoader = useMemo(
+        () => getLogLoader({ analysisId, logType }),
+        [analysisId, logType],
+    );
     const logViewer = useRef(null);
     const loadNext = useCallback(() => {
-        if (logLoader.current) {
-            setLoading(true);
-            logLoader.current.next().then(({ done, value }) => {
-                if (done) {
-                    // Generation is done, close the loader
-                    logLoader.current = null;
-                } else {
-                    setContent((content) => content + value);
-                    setLoading(false);
-                }
-            });
-        }
-    }, [analysisId, logType]);
+        setLoading(true);
+        logLoader.next().then(({ done, value }) => {
+            if (!done) {
+                setContent((content) => content + value);
+            }
+            setLoading(false);
+        });
+    }, [logLoader]);
 
     useEffect(() => {
         setContent("");
         setLoading(false);
-        logLoader.current = getLogLoader({ analysisId, logType });
         loadNext();
-    }, [analysisId, logType]);
+    }, [logLoader]);
 
     return (
         <div style={{ height: "600px" }} className={className}>
@@ -64,10 +66,9 @@ export function LogViewer({
                 ref={logViewer}
                 loading={loading}
                 text={content}
-                onScroll={async ({ scrollHeight, scrollTop, clientHeight }) => {
+                onScroll={({ scrollHeight, scrollTop, clientHeight }) => {
                     if (
                         scrollHeight - scrollTop < clientHeight + 16 &&
-                        logLoader.current &&
                         !loading
                     ) {
                         loadNext();
