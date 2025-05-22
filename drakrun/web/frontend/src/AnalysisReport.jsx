b0d2557b1476a0b4fadcb56954e70e491f6993a8
Paweł Srokosz <pawel.srokosz@cert.pl>
2025-05-22 19:43:24 +0200

    web: Move ProcessTreeView to separate module (#1067)

--- drakrun/web/frontend/src/AnalysisReport.jsx
+++ drakrun/web/frontend/src/AnalysisReport.jsx
@@ -1,93 +1,10 @@
 import { useCallback, useEffect, useState } from "react";
-import { getAnalysisProcessTree, getLog, getLogList } from "./api.js";
-import { ProcessTree } from "./ProcessTree.jsx";
+import { getLog, getLogList } from "./api.js";
 import { TabSwitcher, Tab, useTabContext } from "./TabSwitcher.jsx";
 import { getLogLoader, LogViewer } from "./LogViewer.jsx";
 import { AnalysisMetadataTable } from "./AnalysisMetadataTable.jsx";
 import { AnalysisScreenshotViewer } from "./AnalysisScreenshotViewer.jsx";
-
-function isProcessInteresting(process) {
-    return process.procname.endsWith("explorer.exe");
-}
-
-function getInterestingProcesses(processTree) {
-    let activeSet = new Set();
-    for (let process of processTree) {
-        if (isProcessInteresting(process)) {
-            activeSet.add(process.seqid);
-        }
-        if (process.children.length > 0) {
-            const activeChildren = getInterestingProcesses(process.children);
-            if (activeChildren.size) {
-                activeSet = activeSet.union(activeChildren);
-                activeSet.add(process.seqid);
-            }
-        }
-    }
-    return activeSet;
-}
-
-function ProcessTreeView({ analysisId }) {
-    const [uncollapsed, setUncollapsed] = useState(new Set());
-    const [processTree, setProcessTree] = useState();
-    const [selected, setSelected] = useState();
-    const [error, setError] = useState();
-
-    useEffect(() => {
-        getAnalysisProcessTree({ analysisId })
-            .then((data) => {
-                setProcessTree(data);
-                setUncollapsed(getInterestingProcesses(data));
-            })
-            .catch((e) => {
-                console.error(e);
-                setError(e);
-            });
-    }, []);
-
-    return (
-        <div className="card">
-            <div className="card-body">
-                {typeof processTree === "undefined" ? (
-                    <span>Loading process tree...</span>
-                ) : (
-                    []
-                )}
-                {typeof error !== "undefined" ? (
-                    <span className="text-danger">
-                        Unable to load process tree
-                    </span>
-                ) : (
-                    []
-                )}
-                {typeof processTree !== "undefined" ? (
-                    <ProcessTree
-                        processTree={processTree}
-                        uncollapsedSeqid={uncollapsed}
-                        setCollapse={(seqid) => {
-                            const collapse = uncollapsed.has(seqid);
-                            setUncollapsed((currentValue) => {
-                                let newSet = new Set(currentValue);
-                                if (!collapse) {
-                                    newSet.add(seqid);
-                                } else {
-                                    newSet.delete(seqid);
-                                }
-                                return newSet;
-                            });
-                        }}
-                        selected={selected}
-                        onSelect={(seqid) => {
-                            setSelected(seqid);
-                        }}
-                    />
-                ) : (
-                    []
-                )}
-            </div>
-        </div>
-    );
-}
+import { ProcessTreeView } from "./ProcessTreeView.jsx";
 
 export function AnalysisLogViewerTab({ analysisId }) {
     const logType = useTabContext()?.activeTab;
@@ -170,11 +87,16 @@ function AnalysisReportTabs({ analysis }) {
 }
 
 export function AnalysisReport({ analysis }) {
+    const [selectedProcess, setSelectedProcess] = useState();
     return (
         <>
             <div className="row">
                 <div className="col-6">
-                    <ProcessTreeView analysisId={analysis.id} />
+                    <ProcessTreeView
+                        analysisId={analysis.id}
+                        selectedProcess={selectedProcess}
+                        onProcessSelect={setSelectedProcess}
+                    />
                 </div>
                 <div className="col-6">
                     <AnalysisMetadataTable analysis={analysis} />
