d0b71c3f1c8df9d83062371c16b46f27978067ff
psrok1 <pawel.srokosz@cert.pl>
2025-06-05 14:40:25 +0200

    Compact log index: first steps

--- drakrun/analyzer/postprocessing/indexer.py
+++ drakrun/analyzer/postprocessing/indexer.py
@@ -1,4 +1,5 @@
 import logging
+import msgpack
 import pathlib
 from collections import defaultdict
 from typing import List, Optional
@@ -71,6 +72,34 @@ def index_log_file(
                 logger.exception("Failed to process entry")
     return dict(index)
 
+def build_log_index(
+    analysis_dir: pathlib.Path,
+    process_tree: ProcessTree
+) -> bytes:
+    log_index = [dict() for _ in range(len(process_tree.processes))]
+    for log_file_path in analysis_dir.glob("*.log"):
+        plugin_name = log_file_path.stem
+        if plugin_name == "drakrun":
+            continue
+        index = index_log_file(
+            log_file_path,
+            process_tree,
+            key="Method",
+        )
+        for proc_seqid in index.keys():
+            log_index[proc_seqid][plugin_name] = index[proc_seqid]
+
+    serialized_indexes = []
+    serialized_indexes_current = 0
+    index_toc = [dict() for _ in range(len(process_tree.processes))]
+    for proc_seqid, proc_log_index in enumerate(log_index):
+        for plugin_name, plugin_index in proc_log_index.items():
+            serialized_index = msgpack.packb(plugin_index)
+            index_toc[proc_seqid][plugin_name] = [serialized_indexes_current, serialized_indexes_current + len(serialized_index)]
+            serialized_indexes.append(serialized_index)
+            serialized_indexes_current += len(serialized_index)
+    serialized_index_toc = msgpack.packb(index_toc)
+    return serialized_index_toc + b''.join(serialized_indexes)
 
 def scattered_read_file(
     log_file: pathlib.Path,
