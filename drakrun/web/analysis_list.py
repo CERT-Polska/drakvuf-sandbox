00dcfd3e664050a6fe26d2d3b4d33e1e8f111985
psrok1 <pawel.srokosz@cert.pl>
2025-06-26 14:22:49 +0200

    Fix: add_analysis_to_recent, ISE 500 when there are more analyses than 100

--- drakrun/web/analysis_list.py
+++ drakrun/web/analysis_list.py
@@ -15,11 +15,11 @@ def add_analysis_to_recent(
     pipeline = connection.pipeline(transaction=True)
     pipeline.lpush(ANALYSES_LIST, analysis_id)
     pipeline.lrange(ANALYSES_LIST, ANALYSES_LIST_LENGTH, -1)
-    dropped_ids = pipeline.execute()[-1]
+    dropped_ids: List[bytes] = pipeline.execute()[-1]
     if dropped_ids:
         for id in dropped_ids:
             try:
-                job = Job.fetch(id, connection=connection)
+                job = Job.fetch(id.decode(), connection=connection)
                 if not (job.is_finished or job.is_failed):
                     continue
                 job.delete()
