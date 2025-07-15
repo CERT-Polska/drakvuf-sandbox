5fe91a861b9543d57e84f48993e7c0d806219252
psrok1 <pawel.srokosz@cert.pl>
2025-07-15 17:35:32 +0200

    Add startup info to report

--- drakrun/analyzer/postprocessing/plugins/generate_report.py
+++ drakrun/analyzer/postprocessing/plugins/generate_report.py
@@ -1,9 +1,10 @@
 import logging
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Optional
 
 import orjson
 
+from ..process_tree import ProcessTree
 from .plugin_base import PostprocessContext
 
 logger = logging.getLogger(__name__)
@@ -18,9 +19,47 @@ def get_metadata(analysis_dir: Path) -> Dict:
     return metadata
 
 
+def get_inject_info(analysis_dir: Path, process_tree: Optional[ProcessTree]) -> Dict:
+    inject_file = analysis_dir / "inject.log"
+    if not inject_file.exists():
+        return {}
+    with inject_file.open("r") as f:
+        inject = orjson.loads(f.read().strip())
+    status = inject.get("Status")
+    if status == "Success":
+        pid = inject.get("InjectedPid")
+        if pid and process_tree:
+            process = process_tree.get_process_for_evtid(pid, 0)
+        else:
+            process = None
+        inject_info = {
+            "status": inject["Status"],
+            "process_name": inject.get("ProcessName"),
+            "arguments": inject.get("Arguments"),
+            "pid": pid,
+            "process": process.seqid if process else None,
+        }
+    elif status == "Error":
+        inject_info = {
+            "status": inject["Status"],
+            "error_code": inject.get("ErrorCode"),
+            "error": inject.get("Error"),
+        }
+    else:
+        logger.warning("Unknown status found in inject.log")
+        inject_info = {}
+
+    return inject_info
+
+
 def generate_report(context: PostprocessContext) -> None:
     analysis_dir = context.analysis_dir
-    report = {"info": get_metadata(analysis_dir), **context.report}
+    process_tree = context._process_tree
+    report = {
+        "info": get_metadata(analysis_dir),
+        "startup": get_inject_info(analysis_dir, process_tree),
+        **context.report,
+    }
 
     with (analysis_dir / "report.json").open("wb") as f:
         f.write(orjson.dumps(report, option=orjson.OPT_INDENT_2))
