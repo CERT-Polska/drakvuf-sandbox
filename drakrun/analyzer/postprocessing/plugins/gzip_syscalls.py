c3c8981739c33af1307e24f574ccba88579342b3
psolvx <51050313+psolvx@users.noreply.github.com>
2025-11-17 16:49:43 +0100

    Fix gzip syscalls plugin and also gzip sysret.log (#1143)

--- drakrun/analyzer/postprocessing/plugins/gzip_syscalls.py
+++ drakrun/analyzer/postprocessing/plugins/gzip_syscalls.py
@@ -1,21 +1,26 @@
 import gzip
-import pathlib
 import shutil
 
 from drakrun.lib.config import load_config
 
+from .plugin_base import PostprocessContext
 
-def gzip_syscalls(analysis_dir: pathlib.Path) -> None:
+
+def gzip_syscalls(context: PostprocessContext) -> None:
     """
     Compress syscall.log using gzip if gzip_syscalls configuration option is True.
     This will disable preview of these logs in web.
     """
+    analysis_dir = context.analysis_dir
     config = load_config()
     if not config.drakrun.gzip_syscalls:
         return
-    syscall_log = analysis_dir / "syscall.log"
-    syscall_log_gz = analysis_dir / "syscall.log.gz"
-    with syscall_log.open("rb") as f_in:
-        with gzip.open(syscall_log_gz, "wb") as f_out:
-            shutil.copyfileobj(f_in, f_out)
-    syscall_log.unlink()
+
+    for name in ["syscall", "sysret"]:
+        log_path = analysis_dir / f"{name}.log"
+        log_path_gz = analysis_dir / f"{name}.log.gz"
+        if log_path.exists():
+            with log_path.open("rb") as f_in:
+                with gzip.open(log_path_gz, "wb") as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+        log_path.unlink()
