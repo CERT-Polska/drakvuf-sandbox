428dc168320c0b333b6fd1e3e15e5bb6391ebe0e
psolvx <51050313+psolvx@users.noreply.github.com>
2025-11-17 11:51:06 +0100

    Improve screenshotter thread handling (#1140)

--- drakrun/analyzer/screenshotter.py
+++ drakrun/analyzer/screenshotter.py
@@ -25,6 +25,7 @@ class Screenshotter:
     ):
         self._aioloop = None
         self._thread = None
+        self._task = None
 
         self.output_dir = output_dir
         self.vnc_host = vnc_host
@@ -41,45 +42,57 @@ class Screenshotter:
         screenshot_log_path = self.output_dir / "screenshots.json"
         screenshot_dir = self.output_dir / "screenshots"
         screenshot_dir.mkdir()
-        with screenshot_log_path.open("w") as screenshot_log:
-            async with asyncvnc.connect(
-                host=self.vnc_host, port=self.vnc_port, password=self.vnc_password
-            ) as client:
-                logger.info(f"Connected to VNC {self.vnc_host}:{self.vnc_port}")
-                while screenshot_no < self.max_screenshots:
-                    pixels = await client.screenshot()
-                    timestamp = time.time()
-                    image = Image.fromarray(pixels)
-                    image_hash = hasher.compute(image)
-                    if (
-                        not prev_image_hash
-                        or hasher.compute_distance(image_hash, prev_image_hash)
-                        > self.diff_threshold
-                    ):
-                        prev_image_hash = image_hash
-                        screenshot_no += 1
-                        screenshot_name = (
-                            screenshot_dir / f"screenshot_{screenshot_no}.png"
-                        )
-                        image.save(screenshot_name)
-                        screenshot_log.write(
-                            json.dumps(
-                                {
-                                    "timestamp": timestamp,
-                                    "image_hash": image_hash,
-                                    "index": screenshot_no,
-                                }
+        try:
+            with screenshot_log_path.open("w") as screenshot_log:
+                async with asyncvnc.connect(
+                    host=self.vnc_host, port=self.vnc_port, password=self.vnc_password
+                ) as client:
+                    logger.info(f"Connected to VNC {self.vnc_host}:{self.vnc_port}")
+                    while screenshot_no < self.max_screenshots:
+                        pixels = await asyncio.wait_for(client.screenshot(), timeout=30)
+                        timestamp = time.time()
+                        image = Image.fromarray(pixels)
+                        image_hash = hasher.compute(image)
+                        if (
+                            not prev_image_hash
+                            or hasher.compute_distance(image_hash, prev_image_hash)
+                            > self.diff_threshold
+                        ):
+                            prev_image_hash = image_hash
+                            screenshot_no += 1
+                            screenshot_name = (
+                                screenshot_dir / f"screenshot_{screenshot_no}.png"
                             )
-                            + "\n"
-                        )
-                        logger.info(f"Got screenshot {screenshot_no}: {image_hash}")
-                    await asyncio.sleep(self.loop_interval)
+                            image.save(screenshot_name)
+                            screenshot_log.write(
+                                json.dumps(
+                                    {
+                                        "timestamp": timestamp,
+                                        "image_hash": image_hash,
+                                        "index": screenshot_no,
+                                    }
+                                )
+                                + "\n"
+                            )
+                            logger.info(f"Got screenshot {screenshot_no}: {image_hash}")
+                        await asyncio.sleep(self.loop_interval)
+        except asyncio.CancelledError:
+            logger.info("Screenshot task was cancelled.")
+            # This exception is raised when stop() cancels the task.
+        except Exception as e:
+            logger.exception(f"Error in screenshotter task: {e}")
+        finally:
+            logger.info("Screenshot task finished.")
 
     def perform_loop(self):
+        asyncio.set_event_loop(self._aioloop)
         try:
-            self._aioloop.run_until_complete(self.perform())
+            self._task = self._aioloop.create_task(self.perform())
+            self._aioloop.run_until_complete(self._task)
         finally:
-            self._aioloop.close()
+            if not self._aioloop.is_closed():
+                self._aioloop.close()
+            logger.info("Screenshotter event loop closed.")
 
     def start(self):
         self._aioloop = asyncio.new_event_loop()
@@ -87,5 +100,16 @@ class Screenshotter:
         self._thread.start()
 
     def stop(self):
-        self._aioloop.stop()
-        self._thread.join()
+        if not self._thread:
+            return
+
+        if self._aioloop and not self._aioloop.is_closed() and self._task:
+            logger.info("Stopping screenshotter...")
+            self._aioloop.call_soon_threadsafe(self._task.cancel)
+
+        self._thread.join(timeout=15)
+
+        if self._thread.is_alive():
+            logger.error("Screenshotter thread did not stop in time!")
+
+        logger.info("Screenshotter stopped.")
