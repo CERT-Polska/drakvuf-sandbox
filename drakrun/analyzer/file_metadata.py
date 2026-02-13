bb778228b36b040d68b31644b84d5b86a99ec7a4
psrok1 <pawel.srokosz@cert.pl>
2026-02-13 12:42:02 +0100

    WIP

--- drakrun/analyzer/file_metadata.py
+++ drakrun/analyzer/file_metadata.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from pydantic import BaseModel
+
+from analyzer.analysis_options import AnalysisOptions
 
 
 class FileMetadata(BaseModel):
@@ -8,3 +12,14 @@ class FileMetadata(BaseModel):
 
     def to_dict(self):
         return self.model_dump(mode="json")
+
+
+class AnalysisMetadata(BaseModel):
+    id: str
+    options: AnalysisOptions
+    time_started: str
+    time_finished: Optional[str]
+    time_execution_started: Optional[str]
+    vm_id: Optional[int]
+    file: Optional[FileMetadata]
+    postprocess: Optional[dict]
