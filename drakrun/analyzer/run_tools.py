CERT-Polska/drakvuf-sandbox

commit 1def5459debfd366f14bb71e5c897b5f1e14a214

Feature: allow to set working directory for executed sample (#1167)

drakrun/analyzer/run_tools.py
@@ -77,7 +77,7 @@ def run_drakvuf(
     output_err_file: pathlib.Path,
     drakvuf_args: List[str],
     exec_parameters: Optional[ExecParameters] = None,
-    cwd: Optional[pathlib.Path] = None,
+    drakvuf_cwd: Optional[pathlib.Path] = None,
 ):
     drakvuf_cmdline = get_base_drakvuf_cmdline(
         vm_name,
@@ -89,6 +89,7 @@ def run_drakvuf(
                 exec_cmd=exec_parameters.command,
                 shellexec_args=exec_parameters.shellexec_args,
                 start_method=exec_parameters.start_method,
+                guest_working_dir=exec_parameters.working_dir,
             )
             if exec_parameters
             else {}
@@ -107,7 +108,7 @@ def run_drakvuf(
     try:
         with output_file.open("wb") as output, output_err_file.open("wb") as output_err:
             drakvuf = subprocess.Popen(
-                drakvuf_cmdline, stdout=output, stderr=output_err, cwd=cwd
+                drakvuf_cmdline, stdout=output, stderr=output_err, cwd=drakvuf_cwd
             )
             with process_graceful_exit(drakvuf):
                 yield drakvuf

drakrun/web/frontend/src/api.js
@@ -140,6 +140,8 @@ export async function uploadSample({
     if (file_path) formData.append("file_path", file_path);
     if (start_command) formData.append("start_command", start_command);
     if (start_method) formData.append("start_method", start_method);
+    if (start_working_dir)
+        formData.append("start_working_dir", start_working_dir);
     if (no_internet) formData.append("no_internet", "1");
     if (no_screenshots) formData.append("no_screenshots", "1");
     if (extract_archive) formData.append("extract_archive", "1");

drakrun/analyzer/analysis_options.py
@@ -19,6 +19,9 @@ class AnalysisOptions(BaseModel):
     guest_target_directory: pathlib.PureWindowsPath = pathlib.PureWindowsPath(
         "%USERPROFILE%\\Desktop\\"
     )
+    # Working directory set for the sample during execution
+    # Default is resolved guest_target_directory
+    guest_working_directory: Optional[pathlib.PureWindowsPath] = None
     # Start command to run on the VM
     start_command: Optional[Union[List[str], str]] = None
     # Start method to run start command

drakrun/analyzer/analyzer.py
@@ -251,6 +251,8 @@ def analyze_file(
                 guest_target_directory=options.guest_target_directory,
                 archive_password=options.archive_password,
             )
+            if options.guest_working_directory is None:
+                options.guest_working_directory = target_dir
 
             if options.start_command is None:
                 archive_executable_path = str(
@@ -300,6 +302,13 @@ def analyze_file(
                 injector, options.host_sample_path, str(guest_executable_path)
             )
 
+            resolved_guest_executable_dir = pathlib.PureWindowsPath(
+                guest_executable_path
+            ).parent
+
+            if options.guest_working_directory is None:
+                options.guest_working_directory = resolved_guest_executable_dir
+
             if options.start_command is None:
                 start_method, options.start_command = get_startup_method_and_argv(
                     guest_executable_path, preferred_start_method
@@ -332,6 +341,7 @@ def analyze_file(
                 exec_parameters = make_exec_parameters(
                     options.start_command,
                     start_method,
+                    str(options.guest_working_directory),
                     shellexec_supported,
                 )
                 # At this point options object is used only to visualize
@@ -370,7 +380,7 @@ def analyze_file(
                     drakvuf_err_file,
                     drakvuf_args,
                     exec_parameters=exec_parameters,
-                    cwd=output_dir,
+                    drakvuf_cwd=output_dir,
                 ) as drakvuf,
             ):
                 log.info("Analysis started...")

drakrun/analyzer/startup_command.py
@@ -66,11 +66,13 @@ class ExecParameters(NamedTuple):
     command: str
     shellexec_args: Optional[str]
     full_command: str
+    working_dir: Optional[str]
 
 
 def make_exec_parameters(
     start_command: list[str] | str,
     start_method: StartMethod,
+    working_dir: Optional[str],
     shellexec_supported: bool,
 ) -> ExecParameters:
     shellexec_args = None
@@ -105,4 +107,5 @@ def make_exec_parameters(
         command=exec_cmd,
         shellexec_args=shellexec_args,
         full_command=full_command,
+        working_dir=working_dir,
     )

drakrun/cli/analyze.py
@@ -63,6 +63,20 @@ from .check_root import check_root
     help="Target directory on VM where sample will be copied (Windows path)",
 )
 @click.option(
+    "--guest-archive-entry-path",
+    "guest_archive_entry_path",
+    default=None,
+    type=str,
+    help="File to execute after archive extraction",
+)
+@click.option(
+    "--guest-working-directory",
+    "guest_working_directory",
+    default=None,
+    type=str,
+    help="Alternative working directory to set while executing a file",
+)
+@click.option(
     "--start-command",
     "start_command",
     default=None,
@@ -128,6 +142,8 @@ def analyze(
     preset,
     target_filename,
     target_filepath,
+    guest_archive_entry_path,
+    guest_working_directory,
     start_command,
     start_method,
     plugins,
@@ -179,6 +195,7 @@ def analyze(
         timeout=timeout,
         net_enable=net_enable,
         sample_filename=target_filename,
+        guest_archive_entry_path=guest_archive_entry_path,
         start_command=start_command,
         start_method=start_method,
         plugins=plugins,
@@ -191,6 +208,11 @@ def analyze(
 
     if target_filepath is not None:
         options.guest_target_directory = pathlib.PureWindowsPath(target_filepath)
+
+    if guest_working_directory is not None:
+        options.guest_working_directory = pathlib.PureWindowsPath(
+            guest_working_directory
+        )
 
     metadata = AnalysisMetadata(
         id=output_dir.name,

drakrun/cli/drakvuf_cmdline.py
@@ -33,7 +33,10 @@ log = logging.getLogger(__name__)
     default="createproc",
     help="Execution method for injection (createproc, shellexec, runas)",
 )
-def drakvuf_cmdline(vm_id, cmd, method):
+@click.option(
+    "--working-dir", default=None, help="Working directory for executed sample"
+)
+def drakvuf_cmdline(vm_id, cmd, method, working_dir):
     from drakrun.analyzer.startup_command import make_exec_parameters
 
     config = load_config()
@@ -44,7 +47,9 @@ def drakvuf_cmdline(vm_id, cmd, method):
     if cmd is not None:
         drakvuf_version = get_drakvuf_version()
         supports_shellexec = drakvuf_version.supports_shellexec_verb
-        exec_parameters = make_exec_parameters(cmd, method, supports_shellexec)
+        exec_parameters = make_exec_parameters(
+            cmd, method, working_dir, supports_shellexec
+        )
     else:
         exec_parameters = None
     print(
@@ -58,6 +63,7 @@ def drakvuf_cmdline(vm_id, cmd, method):
                         exec_cmd=exec_parameters.command,
                         shellexec_args=exec_parameters.shellexec_args,
                         start_method=exec_parameters.start_method,
+                        working_dir=exec_parameters.working_dir,
                     )
                     if exec_parameters
                     else {}

drakrun/lib/drakvuf_cmdline.py
@@ -10,6 +10,7 @@ def get_base_drakvuf_cmdline(
     exec_cmd: Optional[str] = None,
     shellexec_args: Optional[str] = None,
     start_method: Optional[str] = None,
+    working_dir: Optional[str] = None,
     extra_args: Optional[List[str]] = None,
 ) -> List[str]:
     args = [
@@ -39,6 +40,8 @@ def get_base_drakvuf_cmdline(
                 exec_args.extend(["-f", shellexec_args])
         else:
             raise ValueError(f"Unsupported start method: {start_method}")
+        if working_dir is not None:
+            exec_args.extend(["-c", working_dir])
         args.extend(
             [
                 "-j",

commit bdf29fa0c71961063e8863d79cb7d98cebd9b65b

Fix: bugs with missing working_dir argument introduced in #1167

drakrun/analyzer/run_tools.py
@@ -89,7 +89,7 @@ def run_drakvuf(
                 exec_cmd=exec_parameters.command,
                 shellexec_args=exec_parameters.shellexec_args,
                 start_method=exec_parameters.start_method,
-                guest_working_dir=exec_parameters.working_dir,
+                working_dir=exec_parameters.working_dir,
             )
             if exec_parameters
             else {}

drakrun/web/frontend/src/api.js
@@ -126,6 +126,7 @@ export async function uploadSample({
     plugins,
     start_command,
     start_method,
+    start_working_dir,
     no_internet,
     no_screenshots,
     extract_archive,